3f6298a1ebbbb12e47acb411c09cdb865d6b836d
KOITT <KOITT@KOITT-PC>
2018-12-22 10:59:13 +0900

    hadoop init

--- src/main/java/com/WordCount.java
+++ src/main/java/com/WordCount.java
@@ -3,6 +3,7 @@ package com;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,7 +15,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.codehaus.jettison.mapped.Configuration;
+
 
 public class WordCount {
 	
